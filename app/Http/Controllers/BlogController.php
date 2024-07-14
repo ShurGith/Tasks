5ef88f0e9bf7ee936ed03f267261e4cc23d5c27f
ShurGith <75266693+ShurGith@users.noreply.github.com>
2024-07-14 15:56:52 +0200

    Iniciando

---
+++ app/Http/Controllers/BlogController.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class BlogController extends Controller
+{
+    public function home(){
+       return view('home');
+    }
+    public function blog(){
+       return view('blog');
+    }
+    public function services(){
+       return view('services');
+    }
+    public function about()
+    {
+        return view('about');
+    }
+    public function contact()
+    {
+        return view('contact');
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -1,9 +1,10 @@
 <?php
 
+use App\Http\Controllers\BlogController;
 use Illuminate\Support\Facades\Route;
 
 Route::view('/', 'welcome');
-
+Route::redirect('/','/dashboard');
 Route::view('dashboard', 'dashboard')
     ->middleware(['auth', 'verified'])
     ->name('dashboard');
@@ -11,5 +12,14 @@ Route::view('dashboard', 'dashboard')
 Route::view('profile', 'profile')
     ->middleware(['auth'])
     ->name('profile');
+
+
+Route::get('/home',[BlogController::class,'home'])->name('home');
+Route::get('/blog',[BlogController::class,'blog'])->name('blog');
+Route::get('/services',[BlogController::class,'services'])->name('services');
+Route::get('/about',[BlogController::class,'about'])->name('about');
+Route::get('/contact',[BlogController::class,'contact'])->name('contact');
+
+
 
 require __DIR__.'/auth.php';
